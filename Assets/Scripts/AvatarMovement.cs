27a0db266e429f7ab90f54b9439f2ce4ba81101c
csalajan <csalajan@nexient.com>
2015-08-25 11:36:28 -0400

    Making the spider walk and jump

--- Assets/Scripts/AvatarMovement.cs
+++ Assets/Scripts/AvatarMovement.cs
@@ -11,14 +11,28 @@ public class AvatarMovement : MonoBehaviour {
 	public Vector3 velocity = Vector3.zero;
 	public Vector3 rotation = Vector3.zero;
 
+    private Animator anim;
+    private float oldPosX;
+    private float oldPosZ;
+    private CharacterController controller;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        oldPosX = transform.position.x;
+        oldPosZ = transform.position.z;
+    }
+
 	void Update() {
-		CharacterController controller = GetComponent<CharacterController>();
-		if (controller.isGrounded) {
+	    
+        if (controller.isGrounded) {
 			velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			velocity = transform.TransformDirection(velocity) * speed;
-			
-			if (Input.GetButton ("Jump")) {
-				velocity.y = jumpSpeed;
+            
+            if (Input.GetButton ("Jump")) {
+                velocity.y = jumpSpeed;
+                
 			}
 		}
 		velocity.y -= gravity * Time.deltaTime;
@@ -26,5 +40,22 @@ public class AvatarMovement : MonoBehaviour {
 		
 		rotation.y = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 		transform.localEulerAngles = rotation;
-	}
+	    if (controller.isGrounded)
+	    {
+	        if (oldPosX != transform.position.x || oldPosZ != transform.position.z)
+	        {
+	            anim.Play("Walk");
+	        }
+	        else
+	        {
+	            anim.Play("Idle");
+	        }
+	    }
+	    else
+	    {
+	        anim.Play("Jump");
+	    }
+	    oldPosX = transform.position.x;
+        oldPosZ = transform.position.z;
+    }
 }
