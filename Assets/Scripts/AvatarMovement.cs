772cb1904e80b7f28101d3f5b3f553187faf25d1
Tyler Fox <tfox@systemsinmotion.com>
2015-08-25 09:03:53 -0400

    Initial commit

---
+++ Assets/Scripts/AvatarMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarMovement : MonoBehaviour {
+	
+	const float speed = 10.0F;
+	const float jumpSpeed = 16.0F;
+	const float gravity = 25.0F;
+	const float sensitivityX = 15F;
+	
+	public Vector3 velocity = Vector3.zero;
+	public Vector3 rotation = Vector3.zero;
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			velocity = transform.TransformDirection(velocity) * speed;
+			
+			if (Input.GetButton ("Jump")) {
+				velocity.y = jumpSpeed;
+			}
+		}
+		velocity.y -= gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+		
+		rotation.y = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+		transform.localEulerAngles = rotation;
+	}
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject Avatar;
+	Transform target;
+	float distance = 12.0F;
+
+	void Start () {
+		target = Avatar.transform;
+	}
+
+	void Update () {
+		transform.position = target.position - target.forward * distance;
+		transform.position = new Vector3 (transform.position.x, transform.position.y + 4.0f, transform.position.z);
+		transform.LookAt(target.position);
+	}
+}
